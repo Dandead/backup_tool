c34ad594921c86c365067914ac433e0105bc4b7a
Dandead <61425314+Dandead@users.noreply.github.com>
2023-07-28 18:28:48 +0300

    fix:fix python dependencies

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-poetry run python3.9 -m backup_tool
+poetry run python3.11 -m backup_tool
--- install.sh
+++ install.sh
@@ -6,14 +6,16 @@ DIR=$( pwd )
 sudo apt update && sudo apt upgrade && sudo apt autoremove
 
 # Install dependencies
-sudo apt install cron python3.9-full python3-distutils python3-apt curl
+sudo apt install cron python3-distutils python3-apt curl software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.11 -y
 
 # Enabale cron
 sudo systemctl enable cron
 sudo systemctl start cron
 
 # Install poetry
-curl -sSL https://install.python-poetry.org | python3 -
+curl -sSL https://install.python-poetry.org | python3.11 -
 
 echo export PATH=\"\$HOME/.local/bin:\$PATH\" >> ~/.zshrc
 echo export PATH=\"\$HOME/.local/bin:\$PATH\" >> ~/.bashrc
