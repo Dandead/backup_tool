8bdddf8b050f436ad7f244c6860340a617c94308
Dandead <61425314+Dandead@users.noreply.github.com>
2023-08-27 15:33:03 +0300

    refactor: replace poerty with pip, make one-command install script

--- install.sh
+++ install.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 
-DIR=$( pwd )
+# Exit if any of commands fails
+set -e
 
 # Update repositories
-sudo apt update && sudo apt upgrade && sudo apt autoremove
+sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove
 
 # Install dependencies
-sudo apt install cron python3-distutils python3-apt curl software-properties-common -y
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install git cron python3-distutils python3-apt python3-pip curl software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install python3.11 -y
+
+# Clone github repository
+git clone https://github.com/Dandead/backup_tool.git
+
+cd backup_tool
+
+DIR=$( pwd )
+
+# Init venv
+python3.11 -m venv venv
+source ./venv/bin/activate
+
+# Update pip
+pip -U pip
+
+# Install project dependencies
+pip install -r requirements.txt
+
+deactivate
 
 # Enabale cron
 sudo systemctl enable cron
 sudo systemctl start cron
 
-# Install poetry
-curl -sSL https://install.python-poetry.org | python3.11 -
-
-echo export PATH=\"\$HOME/.local/bin:\$PATH\" >> ~/.zshrc
-echo export PATH=\"\$HOME/.local/bin:\$PATH\" >> ~/.bashrc
-
-export PATH="$HOME/.local/bin:$PATH"
-
-poetry self update
-
-poetry install
-
 # Add cron task
-echo 0 4 \* \* \* bash $DIR/backup.sh > crontask
+echo 0 4 \*\/3 \* \* bash $DIR/backup.sh > crontask
 crontab crontask
 rm crontask
